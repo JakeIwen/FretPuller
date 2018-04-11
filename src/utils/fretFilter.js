8b58a58430842b86715b6ac324f8b52089cb3cd0
Jake Iwen <iwenx006@umn.edu>
2018-04-10 20:02:41 -0600

    fretfilter

--- src/utils/fretFilter.js
+++ src/utils/fretFilter.js
@@ -1,34 +1,16 @@
-export const fretFilter = ({fretRange, maxFretSpan, incZeroFret, activeStrings, noGaps, allStrings, state, callback}) => {
+// export const fretFilter = ({fretRange, maxFretSpan, incZeroFret, activeStrings, noGaps, allStrings, state, callback}) => {
+export const fretFilter = ({fretRange}) => {
     fretRange = fretRange || state.fretRange
 
-    activeStrings = activeStrings || state.activeStrings
-    incZeroFret = incZeroFret===undefined ? state.incZeroFret : incZeroFret
-
-    console.log({fretRange, maxFretSpan, incZeroFret, activeStrings, noGaps, allStrings})
+    console.log({fretRange})
 
     let newShapes = state.allShapes.filter(fretsInChord => {
-      let crds = fretsInChord.map(fret=>fret.loc.crd)
-      let positions = fretsInChord.map(fret=>fret.loc.pos)
-      let spanOk = (Math.max(...noZeros) - Math.min(...noZeros)) < maxFretSpan
       let rangeOk = noZeros.every( pos => pos >= fretRange[0] && pos < fretRange[1])
-      let stringOk = crds.every( crd => activeStrings[crd])
-      let noGapsOk = !noGaps || crds.every((val, index) => {
-        return index===crds.length-1 || (crds[index+1]-val)===1
-      })
-      let allStringsOk = !allStrings || (crds.length===state.tuning.length)
-
-      return spanOk && rangeOk && stringOk && noGapsOk && allStringsOk
+      return rangeOk
     })
-
     let newState = {
       chordShapes: newShapes,
-      incZeroFret,
-      maxFretSpan,
       fretRange,
-      activeStrings,
-      noGaps,
-      allStrings
     }
-
     callback({newState})
   }
